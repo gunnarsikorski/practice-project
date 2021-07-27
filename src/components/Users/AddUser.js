d69a806fdb10f8672e000c184788d908d1bbc430
gunnarsikorski <gunnarsikorski@gmail.com>
2021-07-27 17:20:17 -0500

    Reformatting AddUser component to use refs instead of state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,46 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-	const [enteredUsername, setEnteredUsername] = useState('');
-	const [enteredAge, setEnteredAge] = useState('');
+	const nameInputRef = useRef();
+	const ageInputRef = useRef();
+
+	// const [enteredUsername, setEnteredUsername] = useState('');
+	// const [enteredAge, setEnteredAge] = useState('');
 	const [error, setError] = useState();
 
 	const addUserHandler = (event) => {
 		event.preventDefault();
-		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+		const enteredName = nameInputRef.current.value
+		const enteredUserAge = ageInputRef.current.value
+		if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
 			setError({
 				title: 'Invalid Input',
 				message: 'Please enter a valid name and age',
 			});
 			return;
 		} // trim removes any whitespace
-		if (+enteredAge < 1) {
+		if (+enteredUserAge < 1) {
 			setError({
 				title: 'Invalid Age',
 				message: 'Please enter a valid age',
 			});
 			return;
 		} // + before a variable forces it to be read as a number not a string
-		props.onAddUser(enteredUsername, enteredAge);
-		setEnteredUsername('');
-		setEnteredAge('');
+		props.onAddUser(enteredName, enteredUserAge);
+		nameInputRef.current.value = ''
+		ageInputRef.current.value = ''
+		// setEnteredUsername('');
+		// setEnteredAge('');
 	};
 
-	const usernameChangeHandler = (event) => {
-		setEnteredUsername(event.target.value);
-	};
-	const ageChangeHandler = (event) => {
-		setEnteredAge(event.target.value);
-	};
+	// const usernameChangeHandler = (event) => {
+	// 	setEnteredUsername(event.target.value);
+	// };
+	// const ageChangeHandler = (event) => {
+	// 	setEnteredAge(event.target.value);
+	// };
 
 	const errorHandler = () => {
 		setError(null);
-        setEnteredUsername('');
-		setEnteredAge('');
+        // setEnteredUsername('');
+		// setEnteredAge('');
 	};
 
 	return (
@@ -58,15 +65,17 @@ const AddUser = (props) => {
 					<input
 						id='username'
 						type='text'
-						value={enteredUsername}
-						onChange={usernameChangeHandler}
+						// value={enteredUsername}
+						// onChange={usernameChangeHandler}
+						ref={nameInputRef}
 					/>
 					<label htmlFor='age'>Age (Years)</label>
 					<input
 						id='age'
 						type='number'
-						value={enteredAge}
-						onChange={ageChangeHandler}
+						// value={enteredAge}
+						// onChange={ageChangeHandler}
+						ref={ageInputRef}
 					/>
 					<Button type='submit'>Add User</Button>
 				</form>
@@ -76,3 +85,5 @@ const AddUser = (props) => {
 };
 
 export default AddUser;
+
+// Commented code is all the state I had before implementing Refs on this component
